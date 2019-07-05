a7a076a69cb6794c4d803fe5e413d0aff1024abd
luanjingqi <luanjingqi@126.com>
2019-07-05 22:31:10 +0800

    加密MD5模块改造

--- routers/admin/index.js
+++ routers/admin/index.js
@@ -5,6 +5,7 @@ let router = new Router()
 router.get('/login', async ctx => {
    await ctx.render('admin/login',{})
 }).post('/login', async ctx=>{
-    let {user , password} = ctx.request.fields;
+    // let {user , password} = ctx.request.fields;
+    ctx.body = '登录成功！'
 })
 module.exports = router.routes()
