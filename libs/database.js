50f742eb5e97b8e96db6dd0d1e10d85d72cf4b2f
luanjingqi <luanjingqi@126.com>
2019-10-31 08:31:51 +0800

    db

--- libs/database.js
+++ libs/database.js
@@ -10,3 +10,70 @@ let conn = mysql.createPool({
 })
 
 module.exports = co(conn)
+methods: {
+    name() {
+        
+    }
+},
+data() {
+    return {
+        key: value
+    }
+},
+created () {
+    ;
+},
+import Vue from 'vue'
+import HelloWorld from './components/HelloWorld'
+
+describe('HelloWorld.vue', () => {
+    it('should render correct contents', () => {
+        const Constructor = Vue.extend(HelloWorld)
+        const vm = new Constructor().mount()
+        expect(vm.el.querySelector('.hello h1').textContent)
+            .to.equal('Welcome to Your Vue.js App')
+    })
+})
+router.afterEach((to, from) => {
+    
+};
+computed: {
+    () {
+        return this.data 
+    }
+},
+import New from '@/components/New.vue';
+import header from '@/components/header.vue';
+actions: {
+    updateValue({commit}, payload) {
+        commit('updateValue', payload);
+    }
+}
+actions: {
+    updateValue({commit}, payload) {
+        commit('updateValue', payload);
+    }
+}
+this.$route.params
+import Name from '@/components/Name.vue'
+
+export default {
+    components: {
+        Name
+    },
+} 
+<template>
+    
+
+</template>
+
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+    state: {
+        key: value
+    }
+});
