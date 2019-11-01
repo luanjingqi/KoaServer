02fb270f40d062545f1777493e27b12f1549740c
luanjingqi <luanjingqi@126.com>
2019-11-01 10:57:38 +0800

    删除

--- routers/www/index.js
+++ routers/www/index.js
@@ -1,6 +1,5 @@
 const Router = require('koa-router')
-const dblink = require('../../libs/db/database')
-const se = require('../../libs/db/dbcontrol')
+const sql = require('../../libs/db/dbcontrol')
 
 
 let router = new Router()
@@ -8,14 +7,14 @@ let router = new Router()
 router.post('login', async ctx=>{
     // console.log(ctx.request.fields)
     // let {q} = ctx.request.fields
-    let result = await se.selectsql('*','luanjingqi.user')
+    let result = await sql.selectsql('*','luanjingqi.user')
     ctx.body = result
 })
 router.get('login', async ctx=>{
     // console.log(ctx.request.fields)
     // let {q} = ctx.request.fields
     
-    let result = await se.selectsql('*','luanjingqi.user')
+    let result = await sql.selectsql('*','luanjingqi.user')
     ctx.body = result
 })
 
