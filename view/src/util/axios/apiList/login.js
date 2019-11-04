cc0ed105791e2c6fa76e3533a02664f97238d1dc
Ljiaji <56952324+Ljiaji@users.noreply.github.com>
2019-11-04 11:27:48 +0800

    。

--- view/src/util/axios/apiList/login.js
+++ view/src/util/axios/apiList/login.js
@@ -1,6 +1,6 @@
 
- import baseURL from './baseURL'; // 导入接口域名列表
- import axios from './http'; // 导入http中创建的axios实例
+ import baseURL from '../baseURL'; // 导入接口域名列表
+ import axios from '../http'; // 导入http中创建的axios实例
  
  const login = {
      // 不带参数
